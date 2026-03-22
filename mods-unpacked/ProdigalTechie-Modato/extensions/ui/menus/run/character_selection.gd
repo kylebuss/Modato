3d1df728b91f284a54d0856c0592ab9c89032781
ProdigalTechie <buss.kyle@gmail.com>
2026-03-22 03:09:56 -0500

    revert to working code

--- mods-unpacked/ProdigalTechie-Modato/extensions/ui/menus/run/character_selection.gd
+++ mods-unpacked/ProdigalTechie-Modato/extensions/ui/menus/run/character_selection.gd
@@ -1,21 +1,30 @@
 extends "res://ui/menus/run/character_selection.gd"
 
-func _on_selections_completed() -> void:
-	print("[ModCharacterSelection] _on_selections_completed called")
-	# replicate base behaviour of adding selected characters
-	for player_index in range(RunData.get_player_count()):
-		var character = _player_characters[player_index]
-		RunData.add_character(character, player_index)
+func _on_element_pressed(element: InventoryElement, _inventory_player_index: int) -> void:
+	var char_select_id = "ProdigalTechie-Modato:CharacterSelect"
+	var customize_stats_scene = "res://mods-unpacked/ProdigalTechie-Modato/extensions/ui/menus/run/customize_stats.tscn"
+	var inventory_player_index = FocusEmulatorSignal.get_player_index(element)
+	if inventory_player_index < 0:
+		return
 
-	if Utils.on_nintendo_nx_or_ounce and RunData.is_coop_run:
-		OS.set_max_controller_count(RunData.get_player_count())
-
-	# Instead of going straight to weapon selection, open customization screen
-	if RunData.some_player_has_weapon_slots():
-		print("[ModCharacterSelection] opening customize_stats.tscn")
-		var packed = preload("res://mods-unpacked/ProdigalTechie-Modato/extensions/ui/menus/run/customize_stats.tscn")
-		var err = get_tree().change_scene_to(packed)
-		print("[ModCharacterSelection] change_scene_to result:", err)
+	if element.is_random:
+		var available_elements := []
+		
+		for element in displayed_elements[0]:
+			if not element.is_locked:
+				available_elements.push_back(element)
+		var character = Utils.get_rand_element(available_elements)
+		_player_characters[inventory_player_index] = character
+	elif element.is_special:
+		return
 	else:
-		RunData.add_starting_items_and_weapons()
-		_change_scene(MenuData.difficulty_selection_scene)
+		_player_characters[inventory_player_index] = null
+		_on_element_focused(element, _inventory_player_index)
+		_player_characters[inventory_player_index] = element.item
+
+	_set_selected_element(inventory_player_index)
+	
+	if get_tree().change_scene(customize_stats_scene) == OK:
+		ModLoaderLog.info("Successfully changed scene!", char_select_id)
+	else:
+		ModLoaderLog.error('Failed loading customize stats scene.', char_select_id)
--- mods-unpacked/ProdigalTechie-Modato/mod_main.gd
+++ mods-unpacked/ProdigalTechie-Modato/mod_main.gd
@@ -5,14 +5,11 @@ const MOD_DIR = MOD_ID + "/"
 
 const EXTS = [
 	"main.gd",
-	"ui/menus/run/character_selection.gd"
 ]
 
 const SINGLETONS = [
-	"item_service.gd",
-	"text_patch.gd"
+	"item_service.gd"
 ]
-
 
 const TRANS = [
 	"translations.de.translation",
